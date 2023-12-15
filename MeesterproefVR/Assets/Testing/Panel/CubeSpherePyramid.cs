f06cf0cce0af3ee68d7e8e97f509007d7a6230a9
Marijn-Jonkers <coolemarijnboy@gmail.com>
2023-12-15 09:47:04 +0100

    All tests done

--- MeesterproefVR/Assets/Testing/Panel/CubeSpherePyramid.cs
+++ MeesterproefVR/Assets/Testing/Panel/CubeSpherePyramid.cs
@@ -1,49 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class CubeSpherePyramid : MonoBehaviour
 {
     public GameObject cube;
-    public string cubeName;
     public GameObject sphere;
-    public string sphereName;
     public GameObject pyramid;
-    public string pyramidName;
 
 
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void HandleInputData(int val)
     {
-        var dropdown = transform.GetComponent<Dropdown>();
-
-        dropdown.options.Clear();
-
-        List<string> items= new List<string>();
-        items.Add(cubeName); 
-        items.Add(sphereName);
-        items.Add(pyramidName);
-
-        foreach(var item in items)
+        if (val == 0)
         {
-            dropdown.options.Add(new Dropdown().OptionData() { text = item});
+            cube.SetActive(true);
+            sphere.SetActive(false);
+            pyramid.SetActive(false);
+        }
+        if (val == 1)
+        {
+            cube.SetActive(false);
+            sphere.SetActive(true);
+            pyramid.SetActive(false);
+        }
+        if (val == 2)
+        {
+            cube.SetActive(false);
+            sphere.SetActive(false);
+            pyramid.SetActive(true);
         }
 
-        dropdown.onValueChanged.AddListener(delegate { DropdownItemsSelected(dropdown); });
-
-    }
-    void DropdownItemsSelected(Dropdown dropdown)
-    {
-        if text
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
