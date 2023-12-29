51dd69b041897f9c81d52c5909ea85c74fd540ba
Marijn-Jonkers <coolemarijnboy@gmail.com>
2023-12-29 14:05:03 +0100

    Created the first version of the bike assembly

---
+++ MeesterproefVR/Assets/Fiets/Scripts/ParentToWithInteraction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ParentToWithInteraction : MonoBehaviour
+{
+    public GameObject parent;
+    public GameObject child;
+    public Vector3 childPos = new Vector3(0, 0, 0);
+    public Vector3 childRot = new Vector3(0, 0, 0);
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == gameObject.tag)
+        {
+            Destroy(child.GetComponent<XRGrabInteractable>());
+            Destroy(child.GetComponent<Rigidbody>());
+            child.transform.SetParent(parent.transform);
+            child.transform.localPosition = childPos;
+            child.transform.localRotation = Quaternion.Euler(childRot);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
